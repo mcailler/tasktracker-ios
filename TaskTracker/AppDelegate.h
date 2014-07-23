18cb5909fa295b84453630d77f2ebd648ebbc8c2
Michael Cailler <cailler.michael@gmail.com>
2014-07-23 09:35:54 -0400

    Initial Commit

---
+++ TaskTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TaskTracker
+//
+//  Created by Michael Cailler on 7/23/14.
+//  Copyright (c) 2014 Brigham & Women's Hospital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TaskTrackerTests/TaskTrackerTests.m
@@ -0,0 +1,34 @@
+//
+//  TaskTrackerTests.m
+//  TaskTrackerTests
+//
+//  Created by Michael Cailler on 7/23/14.
+//  Copyright (c) 2014 Brigham & Women's Hospital. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TaskTrackerTests : XCTestCase
+
+@end
+
+@implementation TaskTrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
